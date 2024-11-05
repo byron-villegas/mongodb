608dd0f4f914cef4293d59134acfff53849287c4
Byron Villegas Moya <96267011+byron-villegas@users.noreply.github.com>
2024-11-05 19:49:55 -0300

    Update Find Exist By Match Element On Array.js

--- Find Exist By Match Element On Array.js
+++ Find Exist By Match Element On Array.js
@@ -1,1 +1,1 @@
-db['whitelist-ips'].find({ Name: "Example", Ips: { $elementMatch: { Ip: "192.168.1.1" } } });
+db['whitelist-ips'].find({ Name: "Example", Ips: { $elemMatch: { Ip: "192.168.1.1" } } });
