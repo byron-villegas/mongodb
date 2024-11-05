90957efa341c4dc2f46778a2487ffa331487e590
Byron Villegas Moya <96267011+byron-villegas@users.noreply.github.com>
2024-11-05 19:50:40 -0300

    Update Create User.js

--- Create User.js
+++ Create User.js
@@ -1,1 +1,1 @@
-db.createUser({ user: "user_example", pwd: passwordPrompt(), roles: [{ role: "readWrite", db: "db_example" }] });
+db.createUser({ user: "user_example", pwd: passwordPrompt(), roles: [{ role: "readWrite", db: "db_example" }] }); // passwordPrompt request you type the password from console
